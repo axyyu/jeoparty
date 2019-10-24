c624bb4880e2b4fa5cfa9c7c61110d2b701ed397
Andrew Wang <andrewxiaoyu0@gmail.com>
2019-10-23 21:52:46 -0400

    Crawler to retrieve categories and count

---
+++ src/utils/GetCategoryByKeyword.js
@@ -0,0 +1,43 @@
+/**
+ * As jService had no way to retrieve categories by query on their API, 
+ * I built a crawler to use the http://jservice.io/search page to obtain
+ * categories using a search term.
+ */
+
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+async function fetchResults(keyword) {
+	const result = await axios.get(`http://jservice.io/search?query=${keyword}`);
+	return cheerio.load(result.data);
+}
+
+function parseResults($) {
+	const rows = $('tbody').find('tr').find('td');
+
+	const categories = [];
+
+	rows.toArray().forEach((obj, index) => {
+		if (index % 2 === 0) {
+			const link = obj.children[0].attribs.href;
+			const start = link.lastIndexOf('/') + 1;
+
+			categories.push({
+				id: link.substring(start),
+				title: obj.children[0].children[0].data
+			});
+		} else {
+			const index = categories.length - 1;
+			const count = obj.children[0].data;
+			categories[index]['clues_count'] = count;
+		}
+	});
+	return categories;
+}
+
+async function getCategoryByKeyword(keyword) {
+	const page = await fetchResults(keyword);
+	parseResults(page);
+}
+
+export default getCategoryByKeyword;
