247d6f50ce8f2552f78778d625bf5eb62182397a
Andrew Wang <andrewxiaoyu0@gmail.com>
2019-11-03 01:06:25 -0500

    Switch to HashRouter

--- src/routes/MainRouter.js
+++ src/routes/MainRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HomePage from './HomePage';
 import SearchPage from './SearchPage';
